ba6379f49da34050abb2d5c884fa52ac61b81cd7
joshdels <deleonjoshy2@gmail.com>
2026-08-30 23:23:06 +0800

    learn: autocad basic visual studio

---
+++ 01-hellocad/01-hellocad/myCommands.cs
@@ -0,0 +1,33 @@
+﻿
+
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using AcadApp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+[assembly: CommandClass(typeof(_01_hellocad.MyCommands))]
+
+namespace _01_hellocad
+{
+    public class MyCommands
+    {
+        [CommandMethod("HELLOCAD")]
+        public void HelloCad() // This method can have any name
+        {
+            // Put your command code here
+            Document doc = 
+                AcadApp.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+
+            if (doc != null)
+            {
+                ed = doc.Editor;
+                ed.WriteMessage("Hello, this is your first command.");
+
+            }
+        }
+
+     
+    }
+}
